e4fa7fa06083dda40e09fa1f9da898cfdfee06bb
Vcvc <vcvckw@gmail.com>
2021-11-08 12:53:24 +0800

    epoll

--- Tars/tc_epoll_server.cpp
+++ Tars/tc_epoll_server.cpp
@@ -2,7 +2,11 @@
 
 namespace tars
 {
-    NetThread::NetThread() {}
+    NetThread::NetThread()
+    {
+        _sock
+    }
+
     NetThread::~NetThread() {}
 
     void NetThread::bind(std::string &ip, int port)
@@ -28,9 +32,70 @@ namespace tars
         } else{
             parseAddr(ip, addr.sin_addr);
         }
+
+        int reuse = 1;
+        setsockopt(_sock, SOL_SOCKET, SO_REUSEADDR, (const void *) &addr, sizeof(int));
+
+        if(::bind(_sock, (struct sockaddr *) &addr, sizeof(addr)) < 0)
+        {
+            std::cerr << "bind error" << std::endl;
+        }
+
+        if(listen(_sock, 100) < 0)
+        {
+            std::cout << "listen error" << std::endl;
+        }
+
+        int flag = 1;
+        if(setsockopt(_sock, SOL_SOCKET, SO_KEEPALIVE, (char*)&flag, sizeof(int)) == -1)
+        {
+            std::cout << "setKeepAlive error" << std::endl;
+        }
+
+        flag=1;
+        if(setsockopt(_sock, IPPROTO_TCP, TCP_NODELAY, (char*)&flag, sizeof(int)) == -1)
+        {
+            std::cout << "[TC_Socket::setTcpNoDelay] error" << std::endl;
+        }
+
+        linger stLinger;
+        stLinger.l_onoff = 1; //在close socket调用后, 但是还有数据没发送完毕的时候容许逗留
+        stLinger.l_linger = 0; //容许逗留的时间为0秒
+
+        if(setsockopt(_sock, SOL_SOCKET, SO_LINGER, (const void *)&stLinger, sizeof(linger)) == -1)
+        {
+            std::cout << "[TC_Socket::setNoCloseWait] error" << std::endl;
+        }
+
+        //设置为非阻塞
+        int val = 0;
+        bool bBlock = false;
+
+        if ((val = fcntl(_sock, F_GETFL, 0)) == -1)
+        {
+            std::cout << "[TC_Socket::setblock] fcntl [F_GETFL] error" << std::endl;
+        }
+        if(!bBlock)
+        {
+            val |= O_NONBLOCK;
+        }
+        else
+        {
+            val &= ~O_NONBLOCK;
+        }
+
+        if (fcntl(_sock, F_SETFL, val) == -1)
+        {
+            std::cout << "fcntl nonblock error" << std::endl;
+        }
+        else
+        {
+            ;
+        }
+
     }
 
-    void NetThread::parseAddr(const int &ip, struct in_addr &seraddr)
+    void NetThread::parseAddr(const std::string &ip, struct in_addr &seraddr)
     {
         int ret = inet_pton(AF_INET, ip.c_str(), &seraddr);
         if(ret < 0)
@@ -44,12 +109,12 @@ namespace tars
             /*
              * http://xsh8637.blog.163.com/blog/static/24099666201081634847407/
              * struct hostent{
-    char *h_name;  //official name
-    char **h_aliases;  //alias list
-    int  h_addrtype;  //host address type
-    int  h_length;  //address lenght
-    char **h_addr_list;  //address list
-}
+                char *h_name;  //official name
+                char **h_aliases;  //alias list
+                int  h_addrtype;  //host address type
+                int  h_length;  //address lenght
+                char **h_addr_list;  //address list
+            }
              */
             char buf[4096] = "\0";
             int iError;
@@ -57,15 +122,21 @@ namespace tars
             gethostbyname_r(ip.c_str(), &stHostent, buf, sizeof(buf), &pstHostent, &iError);
             if (pstHostent == NULL)
             {
-                cout<<"gethostbyname_r error! :"<<endl;
+                std::cout << "gethostbyname_r error! :" << std::endl;
             }
             else
             {
-                stSinAddr = *(struct in_addr *) pstHostent->h_addr;
+                seraddr = *(struct in_addr *) pstHostent->h_addr;
             }
 
         }
 
     }
 
+    void NetThread::createEpoll(uint32_t iIndex)
+    {
+        int _total = 200000;
+
+        _epoller.create(10240);
+    }
 }
--- Tars/tc_epoll_server.h
+++ Tars/tc_epoll_server.h
@@ -2,11 +2,15 @@
 #define LEARN_TARSCPP_TC_EPOLL_SERVER_H
 
 #include <iostream>
-#include <string>
-#include <vector>
-#include <map>
-#include <list>
+#include <cstring>
+#include <cstdlib>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <fcntl.h>
+
 
 #include "tc_epoller.h"
 
@@ -18,11 +22,13 @@ namespace tars
         NetThread();
         ~NetThread();
         void bind(std::string &ip, int port);
-        void createepoll(1);
+        //void createepoll(1);
         void run();
-        void parseAddr(const std::sring &ip, struct in_addr &seraddr);
+        void parseAddr(const std::string &ip, struct in_addr &seraddr);
+        void createEpoll(uint32_t iIndex= 0);
 
     private:
+        TC_Epoller _epoller;
         int _sock;
 
     };
