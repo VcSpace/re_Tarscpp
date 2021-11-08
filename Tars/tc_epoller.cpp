e556072987f88692711a21a9d9b8c7decf10f33d
Vcvc <vcvckw@gmail.com>
2021-11-08 12:53:42 +0800

    epoller

--- Tars/tc_epoller.cpp
+++ Tars/tc_epoller.cpp
@@ -1,1 +1,32 @@
 #include "tc_epoller.h"
+
+namespace tars
+{
+    TC_Epoller::TC_Epoller(bool bET) : _iEpollfd(0), _et(bET), _pevs(nullptr), _max_connections(1024)
+    {
+
+    }
+
+    TC_Epoller::~TC_Epoller()
+    {
+        if(_pevs != nullptr)
+        {
+            delete[] _pevs;
+            _pevs = nullptr;
+        }
+
+        if(_iEpollfd > 0)
+        {
+            close(_iEpollfd);
+        }
+    }
+
+    void TC_Epoller::create(int max_connections)
+    {
+        _max_connections = max_connections;
+        _iEpollfd = epoll_create(_max_connections + 1);
+        delete[] _pevs;
+        _pevs = new epoll_event[_max_connections + 1];
+    }
+
+}
--- Tars/tc_epoller.h
+++ Tars/tc_epoller.h
@@ -1,10 +1,25 @@
 #ifndef LEARN_TARSCPP_TC_EPOLLER_H
 #define LEARN_TARSCPP_TC_EPOLLER_H
 
+#include <sys/epoll.h>
+#include <cassert>
+#include <unistd.h>
 
-class tc_epoller {
+namespace tars
+{
+    class TC_Epoller {
+    public:
+        TC_Epoller(bool bET = true);
+        ~TC_Epoller();
+        void create(int max_connections);
 
-};
+    private:
+        int _iEpollfd;
+        bool _et;
+        struct epoll_event *_pevs;
+        int    _max_connections;
 
+    };
+}
 
-#endif //LEARN_TARSCPP_TC_EPOLLER_H
+#endif //
