43b456a19a1c8c8c305757e14ddbbe998167762c
Vcvc <vcvckw@gmail.com>
2021-11-11 23:53:37 +0800

    .h

---
+++ Tars/socket.h
@@ -0,0 +1,15 @@
+#ifndef TARS_V_SOCKET_H
+#define TARS_V_SOCKET_H
+
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <netinet/tcp.h>
+#include <netdb.h>
+#include <cstdlib>
+#include <
+
+#endif //TARS_V_SOCKET_H
