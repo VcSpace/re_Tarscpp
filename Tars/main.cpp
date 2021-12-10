f7b7bff3f2d49c70039d3ceb5fa788f5d0a84ad4
Vcvc <vcvckw@gmail.com>
2021-12-10 20:46:34 +0800

    init

---
+++ Tars/main.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Vcvc on 2021/12/10.
+//
+
+#include "tc_epoll_server.h"
+
+int main(int argc, char **argv)
+{
+    int port = 8888;
+    std::string ip = "";
+    if(argc > 2)
+    {
+        ip = argv[1];
+        port = atoi(argv[2]);
+    }
+    std::cout << "ReTars Start"<< std::endl;
+    std::cout << "set ip = " << ip << "  " << "set port = " << port << std::endl;
+
+    tars::TC_EpollServer *eserver = new tars::TC_EpollServer();
+    tars::TC_EpollServer::NetThread* vNetThread = eserver->getNetThread();
+
+    vNetThread->bind(ip,port);
+
+
+    std::cout << "close server " << std::endl;
+
+    return 0;
+}
