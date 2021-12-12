c042222f2b98225dc913f63a9d779b5c7bbabb27
Vcvc <vcvckw@gmail.com>
2021-12-12 17:47:50 +0800

    thread

--- Tars/tc_thread.h
+++ Tars/tc_thread.h
@@ -5,9 +5,43 @@
 #include <pthread.h>
 #include <mutex>
 
+#include "tc_monitor.h"
+
 namespace tars
 {
-    class TC_Thread
+    class TC_ThreadControl
+    {
+    public:
+
+        TC_ThreadControl();
+
+        explicit TC_ThreadControl(pthread_t);
+
+        pthread_t id() const;
+
+        void join();
+
+        void detach();
+
+        static void sleep(long millsecond);
+
+        static void yield();
+
+    private:
+
+        pthread_t _thread;
+
+
+    };
+
+    class TC_Runable
+    {
+    public:
+        virtual ~TC_Runable(){};
+        virtual void run() = 0;
+    };
+
+    class TC_Thread : public TC_Runable
     {
     public:
         TC_Thread();
@@ -16,23 +50,7 @@ namespace tars
     private:
     };
 
-    class TC_ThreadMutex
-    {
-    public:
-        TC_ThreadMutex();
-        ~TC_ThreadMutex();
 
-    private:
-    };
-
-    class TC_ThreadCond
-    {
-    public:
-        TC_ThreadCond();
-        ~TC_ThreadCond();
-
-    private:
-    };
 }
 
 #endif //RETARS_TC_THREAD_H
