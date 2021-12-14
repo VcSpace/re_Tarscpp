ac5439bfbef8177154f7d857bada946677d8d254
Vcvc <vcvckw@gmail.com>
2021-12-14 23:04:28 +0800

    monitor

--- Tars/tc_monitor.h
+++ Tars/tc_monitor.h
@@ -2,6 +2,7 @@
 #define RETARS_TC_MONITOR_H
 
 #include "tc_thread_lock.h"
+#include "tc_thread_mutex.h"
 
 namespace tars
 {
@@ -9,12 +10,32 @@ namespace tars
     class TC_Monitor
     {
     public:
+        typedef TC_LockT<TC_Monitor<T, U> > Lock;
+        typedef TC_TryLockT<TC_Monitor<T, U> > TryLock;
+
         TC_Monitor();
         virtual ~TC_Monitor();
+
+        virtual void lock() const;
 
     private:
 
     };
+
+    template<typename T, typename U>
+    TC_Monitor<T, U>::TC_Monitor() {
+
+    }
+
+    template<typename T, typename U>
+    TC_Monitor<T, U>::~TC_Monitor() {
+
+    }
+
+    template<typename T, typename U>
+    void TC_Monitor<T, U>::lock() const {
+
+    }
 
     typedef TC_Monitor<TC_ThreadMutex, TC_ThreadCond> TC_ThreadLock;
 
