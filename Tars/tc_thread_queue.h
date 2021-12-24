9bda8372413ad548c4c6323ce8ce848cf8a0da6c
Vcvc <vcvckw@gmail.com>
2021-12-24 23:41:53 +0800

    queue

--- Tars/tc_thread_queue.h
+++ Tars/tc_thread_queue.h
@@ -14,16 +14,69 @@ namespace tars
     class TC_ThreadQueue : protected TC_ThreadLock
     {
     public:
+        typedef D queue_type;
+
         TC_ThreadQueue() : _size(0) {};
         virtual ~TC_ThreadQueue() {};
 
-        typedef D queue_type;
+        void push_back(const T& t);
+        void push_back(const queue_type &qt);
+        void push_front(const T& t);
+        void push_front(const queue_type &qt);
+
+        bool pop_front(T& t, size_t millsecond = 0);
 
     protected:
         queue_type          _queue;
         size_t              _size;
 
     };
+
+    template<typename T, typename D>
+    bool TC_ThreadQueue<T, D>::pop_front(T &t, size_t millsecond)
+    {
+        Lock lock(*this);
+
+        if (_queue.empty())
+        {
+            if(millsecond == 0)
+            {
+                return false;
+            }
+            if(millsecond == (size_t)-1)
+            {
+                std::cout << "pop_front wait begin" << std::endl;
+                wait();
+                std::cout << "pop_front wait end" << std::endl;
+            }
+            else
+            {
+                //超时了
+                std::cout << "pop_front timedWait begin" << std::endl;
+                if(!timedWait(millsecond))
+                {
+                    return false;
+                }
+            }
+        }
+
+        if(_queue.empty()) //deque
+        {
+            return false;
+        }
+        t = _queue.front();
+        _queue.pop_front();
+        assert(_size > 0);
+        --_size;
+
+        return true;
+
+    }
+
+    template<typename T, typename D>
+    void TC_ThreadQueue<T, D>::push_back(const T &t) {
+
+    }
 
 } //tars
 #endif //RETARS_TC_THREAD_QUEUE_H
