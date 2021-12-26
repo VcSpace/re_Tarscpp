6aa29b048c96c1f61054d7eedbbf0cf7ce2fb1c8
Vcvc <vcvckw@gmail.com>
2021-12-26 23:15:52 +0800

    monitor

--- Tars/tc_monitor.hpp
+++ Tars/tc_monitor.hpp
@@ -1,7 +1,7 @@
-#ifndef RETARS_TC_MONITOR_H
-#define RETARS_TC_MONITOR_H
+#ifndef RETARS_TC_MONITOR_HPP
+#define RETARS_TC_MONITOR_HPP
 
-#include "tc_thread_lock.h"
+#include "tc_thread_lock.hpp"
 #include "tc_thread_mutex.h"
 
 namespace tars
@@ -45,8 +45,12 @@ namespace tars
                 }
             }
         }
-        
+
     private:
+        TC_Monitor(const TC_Monitor&);
+        void operator=(const TC_Monitor&);
+
+    protected:
         mutable int     _nnotify;
         mutable U       _cond;
         T               _mutex;
@@ -98,15 +102,14 @@ namespace tars
     template<typename T, typename U>
     void TC_Monitor<T, U>::unlock() const
     {
-        //notifyImpl(_nnotify);
+        notifyImpl(_nnotify);
         _mutex.unlock();
     }
 
     template<typename T, typename U>
     bool TC_Monitor<T, U>::timedWait(int millsecond) const
     {
-        //notifyImpl(_nnotify);
-
+        notifyImpl(_nnotify);
         bool rc;
 
         try
@@ -123,8 +126,17 @@ namespace tars
         return rc;
     }
 
+    template<typename T, typename U>
+    void TC_Monitor<T, U>::notify()
+    {
+        if(_nnotify != -1)
+        {
+            ++_nnotify;
+        }
+    }
+
 
 } //tars
 
 
-#endif //RETARS_TC_MONITOR_H
+#endif //RETARS_TC_MONITOR_HPP
