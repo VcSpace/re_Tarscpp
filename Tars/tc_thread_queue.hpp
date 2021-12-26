01f928bac49849a5b78125c8b2b93e502e98fbdb
Vcvc <vcvckw@gmail.com>
2021-12-26 23:16:19 +0800

    queue

--- Tars/tc_thread_queue.hpp
+++ Tars/tc_thread_queue.hpp
@@ -1,12 +1,12 @@
-#ifndef RETARS_TC_THREAD_QUEUE_H
-#define RETARS_TC_THREAD_QUEUE_H
+#ifndef RETARS_TC_THREAD_QUEUE_HPP
+#define RETARS_TC_THREAD_QUEUE_HPP
 
 #include <deque>
 #include <vector>
 #include <cassert>
 
-#include "tc_monitor.h"
-#include "tc_thread_lock.h"
+#include "tc_monitor.hpp"
+#include "tc_thread_lock.hpp"
 
 namespace tars
 {
@@ -23,6 +23,7 @@ namespace tars
         void push_back(const queue_type &qt);
         void push_front(const T& t);
         void push_front(const queue_type &qt);
+
         bool swap(queue_type &q, size_t millsecond = 0);
 
         bool pop_front(T& t, size_t millsecond = 0);
@@ -73,10 +74,30 @@ namespace tars
         return true;
 
     }
+    template<typename T, typename D>
+    void TC_ThreadQueue<T, D>::push_back(const T &t)
+    {
+        Lock lock(*this);
+        notify();
+
+        _queue.push_back(t);
+        ++_size;
+    }
 
     template<typename T, typename D>
-    void TC_ThreadQueue<T, D>::push_back(const T &t) {
+    void TC_ThreadQueue<T, D>::push_back(const queue_type &qt)
+    {
+        Lock lock(*this);
 
+        typename queue_type::const_iterator it = qt.begin();
+        typename queue_type::const_iterator itEnd = qt.end();
+        while(it != itEnd)
+        {
+            _queue.push_back(*it);
+            ++it;
+            ++_size;
+            notify();
+        }
     }
 
     template<typename T, typename D>
@@ -116,5 +137,36 @@ namespace tars
         return true;
     }
 
+    template<typename T, typename D>
+    void TC_ThreadQueue<T, D>::push_front(const T &t)
+    {
+        Lock lock(*this);
+
+        notify();
+
+        _queue.push_front(t);
+
+        ++_size;
+
+    }
+
+    template<typename T, typename D>
+    void TC_ThreadQueue<T, D>::push_front(const queue_type &qt)
+    {
+        Lock lock(*this);
+
+        typename queue_type::const_iterator it = qt.begin();
+        typename queue_type::const_iterator itEnd = qt.end();
+
+        while(it != itEnd)
+        {
+            _queue.push_front(*it);
+            ++it;
+            ++_size;
+
+            notify();
+        }
+    }
+
 } //tars
-#endif //RETARS_TC_THREAD_QUEUE_H
+#endif //RETARS_TC_THREAD_QUEUE_HPP
