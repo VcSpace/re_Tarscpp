6b36bcc88672a66071e611c8f85f045a746b3fa0
Vcvc <vcvckw@gmail.com>
2021-12-28 23:12:47 +0800

    init

--- Tars/main.cpp
+++ Tars/main.cpp
@@ -8,29 +8,31 @@ int main(int argc, char **argv)
 {
     int port = 20999;
     std::string ip = "";
-    if(argc > 2)
+    if(argc > 3)
     {
         ip = argv[1];
         port = atoi(argv[2]);
     }
     std::cout << "-----------------\nReTars Start"<< std::endl;
 
-    //std::cout << "set ip = " << ip << "  " << "set port = " << port << std::endl;
+    std::cout << "set ip = " << ip << "  " << "set port = " << port << std::endl;
 
-    tars::TC_EpollServer *eserver = new tars::TC_EpollServer();
-    tars::TC_EpollServer::NetThread* vNetThread = eserver->getNetThread();
+    tars::TC_EpollServerPtr _epollServer = std::make_shared<tars::TC_EpollServer>();
+    tars::TC_EpollServer::NetThread* vNetThread = _epollServer->getNetThread();
 
-    vNetThread->bind(ip,port);
-    vNetThread->createEpoll(1);
+    tars::TC_EpollServer::BindAdapterPtr lsPtr = std::make_shared<tars::TC_EpollServer::BindAdapter>(_epollServer.get());
+    //lsPtr->setEndpoint(ip,port);
 
-    tars::TC_EpollServer::Handle handle;
-    handle.setEpollServer(eserver);
-    handle.start();
-
-    vNetThread->run();
+//    //vNetThread->bind(ip,port);
+//    vNetThread->createEpoll(1);
+//
+//    tars::TC_EpollServer::Handle handle;
+//    handle.setEpollServer(_epollServer.get());
+//    handle.start();
+//
+//    vNetThread->run();
 
     std::cout << "-----------------\nReTars Exit" << std::endl;
-    delete eserver;
 
     return 0;
 }
